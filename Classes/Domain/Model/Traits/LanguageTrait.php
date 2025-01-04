7d68df2fcccdba85c15ee7c93c8bf4930899e540
metacontext <Torsten.Schrade@adwmainz.de>
2025-01-04 21:28:20 +0100

    Add new Trait file

---
+++ Classes/Domain/Model/Traits/LanguageTrait.php
@@ -0,0 +1,53 @@
+<?php
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  Copyright (C) 2024 Academy of Sciences and Literature | Mainz
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+namespace Digicademy\Academy\Domain\Model\Traits;
+
+use TYPO3\CMS\Extbase\Annotation\Validate;
+
+/**
+ * Provides the sys language uid of the instance
+ *
+ * @author Torsten Schrade <torsten.schrade@adwmainz.de>
+ */
+trait LanguageTrait
+{
+    /**
+     * @var int|null
+     */
+    protected ?int $sysLanguageUid;
+
+    /**
+     * Returns the sys_language_uid
+     *
+     * @return int|null $sysLanguageUid
+     */
+    public function getSysLanguageUid(): ?int
+    {
+        return $this->sysLanguageUid;
+    }
+
+}
