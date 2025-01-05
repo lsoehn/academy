ef9aadefd672d34509c9b5239697333fad5314f5
metacontext <Torsten.Schrade@adwmainz.de>
2025-01-05 08:47:06 +0100

    Remove the old plugin flexforms altogether

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -28,32 +28,8 @@ ExtensionManagementUtility::addPiFlexFormValue('academy_list', 'FILE:EXT:academy
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['academy_show'] = 'pi_flexform';
 ExtensionManagementUtility::addPiFlexFormValue('academy_show', 'FILE:EXT:academy/Configuration/FlexForms/ShowPlugin.xml');
 
-
-
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['academy_projects'] = 'pi_flexform';
-ExtensionManagementUtility::addPiFlexFormValue('academy_projects', 'FILE:EXT:academy/Configuration/FlexForms/ProjectsPlugin.xml');
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['academy_units'] = 'pi_flexform';
-ExtensionManagementUtility::addPiFlexFormValue('academy_units', 'FILE:EXT:academy/Configuration/FlexForms/UnitsPlugin.xml');
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['academy_persons'] = 'pi_flexform';
-ExtensionManagementUtility::addPiFlexFormValue('academy_persons', 'FILE:EXT:academy/Configuration/FlexForms/PersonsPlugin.xml');
-
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['academy_media'] = 'pi_flexform';
 ExtensionManagementUtility::addPiFlexFormValue('academy_media', 'FILE:EXT:academy/Configuration/FlexForms/MediaPlugin.xml');
 
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['academy_mediaviewer'] = 'pi_flexform';
 ExtensionManagementUtility::addPiFlexFormValue('academy_mediaviewer', 'FILE:EXT:academy/Configuration/FlexForms/MediaviewerPlugin.xml');
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['academy_hcards'] = 'pi_flexform';
-ExtensionManagementUtility::addPiFlexFormValue('academy_hcards', 'FILE:EXT:academy/Configuration/FlexForms/HcardsPlugin.xml');
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['academy_products'] = 'pi_flexform';
-ExtensionManagementUtility::addPiFlexFormValue('academy_products', 'FILE:EXT:academy/Configuration/FlexForms/ProductsPlugin.xml');
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['academy_services'] = 'pi_flexform';
-ExtensionManagementUtility::addPiFlexFormValue('academy_services', 'FILE:EXT:academy/Configuration/FlexForms/ServicesPlugin.xml');
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['academy_publications'] = 'pi_flexform';
-ExtensionManagementUtility::addPiFlexFormValue('academy_publications', 'FILE:EXT:academy/Configuration/FlexForms/PublicationsPlugin.xml');
